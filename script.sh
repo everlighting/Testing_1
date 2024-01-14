d88dc1cfb028d98d0d1c01e93c9da9bffe876044
Akshay <akshayretnakumar8089886350@gmail.com>
2024-01-14 11:59:10 +0530

    adding external script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install cowsay -y
-cowsay -f ghost "Iam a COW ... Run for your life" >> dragon.txt
+cowsay -f dragon "Iam a COW ... Run for your life" >> dragon.txt
 grep -i dragon "dragon.txt"
  ls -ltra
  cat dragon.txt
